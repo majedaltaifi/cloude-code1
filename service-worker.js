28e941de9e0ea9d72ebdd7c0b918fde2cfc2e531
majedaltaifi <153317619+majedaltaifi@users.noreply.github.com>
2026-04-09 11:02:58 +0300

    feat: implement PWA support with manifest, service worker and icons

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nit-field-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@500;900&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
