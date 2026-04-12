18cf53d43964e69e11cae8a3203a1640f39f7873
majedaltaifi <153317619+majedaltaifi@users.noreply.github.com>
2026-04-12 15:24:32 +0300

    fix: implement aggressive PWA content refresh to eliminate old cached versions

--- service-worker.js
+++ service-worker.js
@@ -33,10 +33,25 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
